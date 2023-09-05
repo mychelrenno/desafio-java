ee3f07149fdb7f5e0696c4a0de264d1e292a2e3e
mychel.renno <mychel.renno@wises.com.br>
2023-09-05 09:43:59 -0300

    configurado spring test

--- desafio-java/src/main/test/br/com/biblioteca/service/PessoaServiceTest.java
+++ desafio-java/src/main/test/br/com/biblioteca/service/PessoaServiceTest.java
@@ -1,20 +1,26 @@
 package br.com.biblioteca.service;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
-//@SpringBootTest
+@SpringBootTest
 class PessoaServiceTest {
 	
-//	@Autowired
-//    private MockMvc mvc;
+	@Autowired
+    private MockMvc mvc;
 	
-//	@MockBean
-//	PessoaService pessoaService;
+	@MockBean
+	PessoaService pessoaService;
 	
 	@Test
-	void test() {
-//		PessoaService mock = Mockito.mock(PessoaService.class);
+	void testeBasicoDeSoma() {
+		int soma = 5 + 1;
+		assertEquals(6, soma);
 	}
 
 }
